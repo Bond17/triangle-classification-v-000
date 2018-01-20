a46e3ea5945cb63f01a0fbb09c4c62c449405794
Robert Watson <spartan83@gmail.com>
2018-01-20 21:56:56 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,8 +12,12 @@ class Triangle
   
   def kind 
     
-    if
-      raise
+    if (side1 <=0 || side2 <=0 || side3 <=0 ||side1+side2 <= side3 || side2+side3 <= side1 || side1+side3 <= side2 )
+      begin
+        raise TriangleErrorError
+      rescue TriangleError => error
+          puts error.message
+      end
     end
     
     if @side1==@side2 && @side2 ==@side3
