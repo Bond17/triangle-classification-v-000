7e1ea45885c6b05dcd5c5981bf692b8a00ca1cd9
Robert Watson <spartan83@gmail.com>
2018-01-20 21:58:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
     
     if (@side1 <=0 || @side2 <=0 || @side3 <=0 ||@side1+@side2 <= @side3 || @side2+@side3 <= @side1 || @side1+@side3 <= @side2 )
       begin
-        raise TriangleErrorError
+        raise TriangleError
       rescue TriangleError => error
           puts error.message
       end
